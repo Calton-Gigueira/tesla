45a127d9ba75eeb90c3d9db9518e592d0413ad6f
Calton Gigueira <caltongigueira@gmail.com>
2024-05-23 00:02:56 +0100

    Finish the menu

---
+++ scripts/menu-items.js
@@ -0,0 +1,33 @@
+export const menuButtons = [
+  {
+    name: "Vehicles",
+    arrow: "arrow_forward_ios",
+  },
+  {
+    name: "Energy",
+    arrow: "arrow_forward_ios",
+  },
+  {
+    name: "Charging",
+    arrow: "arrow_forward_ios",
+  },
+  {
+    name: "Discover",
+    arrow: "arrow_forward_ios",
+  },
+  {
+    name: "Shop",
+  },
+  {
+    name: "Support",
+  },
+  {
+    name: "United States",
+    arrow: "arrow_forward_ios",
+    icon: 'language'
+  },
+  {
+    name: "Account",
+    icon: 'account_circle'
+  },
+];
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,11 +1,54 @@
+import { menuButtons } from "./menu-items.js";
+
 const closeButton = document.querySelector(".close-button");
 const menuButton = document.querySelector(".menu-button");
 const hiddenMenu = document.querySelector(".hidden-menu");
 
 menuButton.addEventListener('click', () => {
   hiddenMenu.style.width = '100%';
+  closeButton.style.display = 'block';
 });
 
 closeButton.addEventListener('click', () => {
   hiddenMenu.style.width = "0";
+  closeButton.style.display = "none";
 });
+
+
+const menu = document.querySelector("#menu-items");
+
+function renderMenuItems(menuButtons) {
+  let html;
+
+  menuButtons.map((el) => {
+    html = `
+      <button>
+        <span class="material-symbols-outlined ic"
+          style="display: ${
+            el.name === "United States" || el.name === "Account"
+              ? "block"
+              : "none"
+          }"
+        >
+          ${el.icon}
+        </span>
+        <span class="name ${el.name === "United States" ? "us" : ""} 
+          ${el.name === "Account" ? "acc" : ""}
+        ">${el.name} </span>
+        <span class="material-symbols-outlined arrow"
+          style="display: ${
+            el.name === "Shop" || el.name === "Support" || el.name === "Account"
+              ? "none"
+              : ""
+          }"
+        >
+          ${el.arrow}
+        </span>
+      </button>
+    `;
+
+    menu.innerHTML += html;
+  });
+}
+
+renderMenuItems(menuButtons);
